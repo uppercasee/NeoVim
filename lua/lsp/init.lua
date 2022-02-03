ff0bc6611f94f8ba610d8c2c72a99ad2344ca20c
ekloyatri <70530668+ekloyatri@users.noreply.github.com>
2022-02-03 10:04:32 +0545

    added lsp/init

---
+++ lua/lsp/init.lua
@@ -0,0 +1,24 @@
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+local status_ok, coq = pcall(require, "coq")
+if not status_ok then
+  return
+end
+
+require("lsp.lsp-installer")
+require("lsp.handlers").setup()
+-- require("settings.lsp.null-ls")
+
+-- Automatically start coq
+vim.g.coq_settings = { auto_start = 'shut-up' }
+
+-- Enable some language servers with the additional completion capabilities offered by coq_nvim
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities({
+    -- on_attach = my_custom_on_attach,
+  }))
+end
