4ddb5953c4aa5cc89fe0e4d22f8dfbd39e977795
ekloyatri <70530668+ekloyatri@users.noreply.github.com>
2022-02-03 20:03:53 +0545

    Updated keymaps

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -35,13 +35,13 @@ local options = {
     -- guifont = "monospace:h17",               -- the font used in graphical neovim applications
     -- shell = "powershell"                     -- change the shell inside terminal of nvim (bash/cmd/powershell)
   }
-  
+
   vim.opt.shortmess:append "c"
-  
+
   for k, v in pairs(options) do
     vim.opt[k] = v
   end
-    
+
   vim.cmd "set whichwrap+=<,>,[,],h,l"
   vim.cmd [[set iskeyword+=-]]
   vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
