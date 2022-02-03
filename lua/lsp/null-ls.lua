280ece1317fd449ae80cbec3d5cb1345aa2d881a
ekloyatri <70530668+ekloyatri@users.noreply.github.com>
2022-02-03 19:57:19 +0545

    Updated null-ls

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -16,4 +16,10 @@ null_ls.setup({
 		formatting.stylua,
     diagnostics.flake8
 	},
+	-- -- NOTE: You can remove this on attach function to disable format on save
+    -- on_attach = function(client)
+	-- 	if client.resolved_capabilities.document_formatting then
+	-- 	  vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+	-- 	end
+	-- end,
 })
