ec576f265da92c18929fe679d9316290a16a168f
ekloyatri <70530668+ekloyatri@users.noreply.github.com>
2022-02-04 13:50:57 +0545

    minor change

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -108,7 +108,7 @@ return require('packer').startup(function()
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "glepnir/lspsaga.nvim" -- lspsaga
 
   -- for AutoCompletion
