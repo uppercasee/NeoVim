4a0247b719b1a057e153d3f3c03e635cd2462674
3kl0y47r1 <70530668+3kl0y47r1@users.noreply.github.com>
2022-05-10 14:12:49 +0545

    Added © to dashboard

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "3kl0y47r1"
+	return "© 3kl0y47r1"
 end
 
 dashboard.section.footer.val = footer()
