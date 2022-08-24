d24321040e4dec3cbeda460ad733458b328c29d5
uppercasee <70530668+uppercasee@users.noreply.github.com>
2022-08-24 11:08:27 +0545

    updated init file

--- init.lua
+++ init.lua
@@ -1,45 +1,36 @@
-local sources = {
 	-- settings
-	"settings.colorschemes",
-	"settings.keymaps",
-	"settings.options",
-	"settings.plugins",
-	"settings.impatient",
-
-	-- plugins
-	"configs.alpha",
-	"configs.autocommands",
-	"configs.autopairs",
-	"configs.autosave",
-	"configs.better_escape",
-	"configs.bufferline",
-	"configs.cmp",
-	"configs.colorizer",
-	"configs.comments",
-	"configs.dap",
-	"configs.filetype",
-	"configs.gitsigns",
-	-- "configs.icons",
-	"configs.illuminate",
-	"configs.indentline",
-	"configs.lualine",
-	"configs.neoscroll",
-	"configs.nvim-tree",
-	"configs.presence",
-	"configs.project",
-	"configs.symbol-outline",
-	"configs.telescope",
-	"configs.toggleterm",
-	"configs.treesitter",
-	"configs.trouble",
-	"configs.webdev-icons",
-	"configs.whichkey",
-	"lsp",
-}
-
-for _, source in ipairs(sources) do
-	local status_ok, notok = pcall(require, source)
-	if not status_ok then
-		vim.notify("Couldn't load " .. source .. "\n" .. notok)
-	end
-end
+require("settings.colorschemes")
+require("settings.keymaps")
+require("settings.options")
+require("settings.plugins")
+require("settings.impatient")
+-- plugins
+require("configs.alpha")
+require("configs.autocommands")
+require("configs.autopairs")
+require("configs.autosave")
+require("configs.better_escape")
+require("configs.bufferline")
+require("configs.cmp")
+require("configs.colorizer")
+require("configs.comments")
+require("configs.dap")
+require("configs.filetype")
+require("configs.gitsigns")
+require("configs.icons")
+require("configs.illuminate")
+require("configs.indentline")
+require("configs.lualine")
+require("configs.neoscroll")
+require("configs.nvim-tree")
+require("configs.presence")
+require("configs.project")
+require("configs.symbol-outline")
+require("configs.telescope")
+require("configs.toggleterm")
+require("configs.treesitter")
+require("configs.trouble")
+require("configs.webdev-icons")
+require("configs.whichkey")
+-- LSP
+require("lsp")
