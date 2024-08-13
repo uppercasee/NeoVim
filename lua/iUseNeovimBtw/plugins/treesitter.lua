5fd150db502c2313f7e73fef9ff4ae515d6198e0
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-08-13 13:14:44 +0545

    updated treesitter and telescope

--- lua/iUseNeovimBtw/plugins/treesitter.lua
+++ lua/iUseNeovimBtw/plugins/treesitter.lua
@@ -1,11 +1,20 @@
 return {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      branch = "main",
-      lazy = false, 
-      config = function()
-        require("iUseNeovimBtw.treesitter").setup()
-      end,
-    },
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		branch = "main",
+		lazy = false,
+		opts = {
+			ensure_installed = "all",
+			sync_install = false,
+			auto_install = true,
+			indent = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "markdown" },
+			},
+		},
+	}
+}
