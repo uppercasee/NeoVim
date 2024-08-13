cebf2c243ac7c2381323536be094690fb6dfdcc4
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-08-13 11:44:09 +0545

    Added keymaps and options

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -66,3 +66,6 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- -- NvimTreeToggle
+-- keymap("n", "\\", ":NvimTreeToggle<cr>", opts)
