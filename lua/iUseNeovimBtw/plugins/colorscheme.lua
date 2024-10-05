d01bf4724d92b9cb98c98ece833d4c76544aee4b
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-10-05 20:13:42 +0545

    changed colorscheme to dracula

--- lua/iUseNeovimBtw/plugins/colorscheme.lua
+++ lua/iUseNeovimBtw/plugins/colorscheme.lua
@@ -1,20 +1,30 @@
 return {
+
+	{
+		"Mofiqul/dracula.nvim",
+		priority = 1000,
+		event = "VimEnter",
+		config = function()
+			vim.cmd("colorscheme dracula")
+		end,
+	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000,
-		event = "VimEnter",  -- Load on VimEnter
-		opts = {
-			flavour = "macchiato", -- latte, frappe, macchiato, mocha
-			background = { dark = "mocha" },
-		},
-		config = function()
-			vim.cmd("colorscheme catppuccin")
-		end,
+    lazy = true,
+		-- priority = 1000,
+		-- event = "VimEnter", -- Load on VimEnter
+		-- opts = {
+		-- 	flavour = "macchiato", -- latte, frappe, macchiato, mocha
+		-- 	background = { dark = "mocha" },
+		-- },
+		-- config = function()
+		-- 	vim.cmd("colorscheme catppuccin")
+		-- end,
 	},
-	{ "tanvirtin/monokai.nvim", lazy=true},
-	{ "Mofiqul/dracula.nvim" , lazy=true},
-	{ "lunarvim/darkplus.nvim" , lazy=true},
-	{ "lunarvim/colorschemes" , lazy=true},
-	{ "navarasu/onedark.nvim" , lazy=true},
+	{ "tanvirtin/monokai.nvim", lazy = true },
+	{ "Mofiqul/dracula.nvim", lazy = true },
+	{ "lunarvim/darkplus.nvim", lazy = true },
+	{ "lunarvim/colorschemes", lazy = true },
+	{ "navarasu/onedark.nvim", lazy = true },
 }
