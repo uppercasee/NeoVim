704643412b826be7b8b73ab3bf40fc778894f0a7
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-10-06 09:29:36 +0545

    updated colorscheme

--- lua/iUseNeovimBtw/plugins/colorscheme.lua
+++ lua/iUseNeovimBtw/plugins/colorscheme.lua
@@ -1,27 +1,40 @@
 return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        event = "VimEnter", -- Load on VimEnter
+        opts = {
+            flavour = "macchiato", -- latte, frappe, macchiato, mocha
+            background = { dark = "mocha" },
+        },
+    },
+    {
+        "tanvirtin/monokai.nvim",
+        priority = 1000,
+        event = "VimEnter",
+    },
+    {
+        "lunarvim/darkplus.nvim",
+        priority = 1000,
+        event = "VimEnter",
+    },
+    {
+        "lunarvim/colorschemes",
+        priority = 1000,
+        event = "VimEnter",
+    },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        event = "VimEnter",
+    },
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		-- priority = 1000,
-		-- event = "VimEnter", -- Load on VimEnter
-		-- opts = {
-		-- 	flavour = "macchiato", -- latte, frappe, macchiato, mocha
-		-- 	background = { dark = "mocha" },
-		-- },
-		-- config = function()
-		-- 	vim.cmd("colorscheme catppuccin")
-		-- end,
-	},
-	{
-		"Mofiqul/dracula.nvim",
-		priority = 1000,
-		event = "VimEnter",
-		config = function()
-			vim.cmd("colorscheme dracula")
-		end,
-	},
-	{ "tanvirtin/monokai.nvim" },
-	{ "lunarvim/darkplus.nvim" },
-	{ "lunarvim/colorschemes" },
-	{ "navarasu/onedark.nvim" },
+        "Mofiqul/dracula.nvim",
+        priority = 1000,
+        event = "VimEnter",
+        config = function()
+            vim.cmd("colorscheme dracula")
+        end,
+    },
 }
