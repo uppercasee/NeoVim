421f4cf2d83283440a94ac91690fbd5d419978d3
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-10-06 10:08:56 +0545

    updated hop

--- lua/iUseNeovimBtw/plugins/hop.lua
+++ lua/iUseNeovimBtw/plugins/hop.lua
@@ -1,65 +1,14 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("", "<leader>l", ":HopWordCurrentLine<cr>", { silent = true })
-keymap("", "<leader>h", ":HopPattern<cr>", { silent = true })
-
-keymap(
-	"o",
-	"f",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>",
-	opts
-)
-keymap(
-	"o",
-	"F",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>",
-	opts
-)
-keymap(
-	"o",
-	"t",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>",
-	opts
-)
-keymap(
-	"o",
-	"T",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>",
-	opts
-)
-
-keymap(
-	"n",
-	"f",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>",
-	opts
-)
-keymap(
-	"n",
-	"F",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>",
-	opts
-)
-keymap(
-	"n",
-	"t",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false})<CR>",
-	opts
-)
-keymap(
-	"n",
-	"T",
-	":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false})<CR>",
-	opts
-)
+keymap("", "<leader>hl", ":HopWordCurrentLine<cr>", { silent = true, desc = "[H]op Current [L]ine" })
+keymap("", "<leader>hp", ":HopPattern<cr>", { silent = true, desc = "[H]op [P]attern" })
 
 return {
-	"hadronized/hop.nvim",
-	branch = "v2", -- optional but strongly recommended
-	cmd = { "HopWord", "HopChar1", "HopChar2", "HopLine", "HopPattern" },  -- Load on specific commands
-  -- event = "VeryLazy",
+	"smoka7/hop.nvim",
+	cmd = { "HopWord", "HopChar1", "HopChar2", "HopLine", "HopPattern", "HopWordCurrentLine" }, -- Load on specific commands
 	config = function()
-		require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+		local hop = require("hop")
+		hop.setup({ keys = "etovxqpdygfblzhckisuran" })
 	end,
 }
