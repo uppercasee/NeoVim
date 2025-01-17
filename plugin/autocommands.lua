2757b3320c617e3d1525a37d529a3bb8c5eaafa1
uppercasee <70530668+uppercasee@users.noreply.github.com>
2025-01-17 13:48:04 +0545

    Autocommands

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -23,12 +23,14 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- removing trailing whitespace on save
 vim.api.nvim_create_autocmd("BufWritePre", {
+	desc = "removing trailing whitespace on save",
 	pattern = "*",
 	command = [[%s/\s\+$//e]],
 })
 
 -- close on 'q'
 vim.api.nvim_create_autocmd("FileType", {
+	desc = "Close on q",
 	pattern = { "help", "man", "qf", "lspinfo", "startuptime", "checkhealth" },
 	command = "nnoremap <buffer> q :close<CR>",
 })
@@ -44,3 +46,43 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
 --         require("nvim-highlight-colors").turnOn()
 --     end,
 -- })
+
+-- Show cursor line only in active window
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+	desc = "Show cursor line only in active window",
+	callback = function()
+		vim.wo.cursorline = true
+	end,
+})
+
+-- Remove cursor line from inactive window
+vim.api.nvim_create_autocmd("WinLeave", {
+	desc = "Remove cursor line from inactive windows",
+	callback = function()
+		vim.wo.cursorline = false
+	end,
+})
+
+-- Disable relative numbers in insert mode
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+	desc = "Disable relative numbers in insert mode",
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
+
+-- Enable relative numbers in insert mode
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+	desc = "Enable relative numbers in normal mode",
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
+
+-- Highlight line exceeding 80 characters
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	desc = "Highlight lines exceeding 80 characters",
+	callback = function()
+		vim.cmd("match ErrorMsg '\\%81v.+'")
+	end,
+})
